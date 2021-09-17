c7c2db216314990c30bbcb780be605b60630a556
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-17 09:53:55 -0400

    :sparkles: adds angular material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ResumeRouterModule } from './temporal-flow/resume-router.module';
@@ -14,6 +16,10 @@ import { WipWelcomeComponent } from './temporal-flow/wip-welcome/wip-welcome.com
     BrowserModule,
     BrowserAnimationsModule,
     ResumeRouterModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ],
   bootstrap: [AppComponent],
 })
