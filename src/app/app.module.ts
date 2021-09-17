b1a6a0ee9e34d574b85104396b7c41c492b4479a
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-17 09:53:33 -0400

    :art: formats code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 import { ResumeRouterModule } from './temporal-flow/resume-router.module';
 import { InDevelopmentComponent } from './temporal-flow/in-development/in-development.component';
 import { WipWelcomeComponent } from './temporal-flow/wip-welcome/wip-welcome.component';
@@ -14,7 +15,6 @@ import { WipWelcomeComponent } from './temporal-flow/wip-welcome/wip-welcome.com
     BrowserAnimationsModule,
     ResumeRouterModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
