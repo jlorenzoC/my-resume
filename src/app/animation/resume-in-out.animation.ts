14693f760822de457adb867bf6873b10977c309f
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-18 14:33:35 -0400

    :recycle: outsources animation to its own file

---
+++ src/app/animation/resume-in-out.animation.ts
@@ -0,0 +1,8 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+
+export const inOutAnimation = trigger('resume_inOut', [
+  transition('* <=> *', [
+    style({ opacity: 0 }),
+    animate('0.2s', style({ opacity: 1 })),
+  ]),
+]);
