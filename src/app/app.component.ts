e4b25a9fafba0c2e2f2bfe5dfb69fe44b68422f0
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-18 15:42:01 -0400

    :fire: removes unecesary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { inOutAnimation } from './animation/resume-in-out.animation';
 
 @Component({
@@ -8,12 +7,6 @@ import { inOutAnimation } from './animation/resume-in-out.animation';
   styleUrls: ['./app.component.scss'],
   animations: [inOutAnimation],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'my-resume';
-  constructor(public breakpointObserver: BreakpointObserver) {}
-  ngOnInit(): void {
-    this.breakpointObserver
-      .observe([Breakpoints.XSmall])
-      .subscribe(console.log);
-  }
 }
