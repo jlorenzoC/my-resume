39fe3a2f8df375c8b3cf1d25c3e4b1cc54594581
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-22 10:26:25 -0400

    :sparkles: adds Angular pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TemporalResumeModule } from './temporal-flow-module/temporal-resume.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, TemporalResumeModule],
+  imports: [BrowserModule, BrowserAnimationsModule, TemporalResumeModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
